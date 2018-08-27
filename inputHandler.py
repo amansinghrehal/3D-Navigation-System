a2b5d21f6692b0275cc3b3283ba021f6a5dfeb14
amansinghrehal <42092944+amansinghrehal@users.noreply.github.com>
2018-08-27 17:17:51 +0530

    Update inputHandler.py

--- inputHandler.py
+++ inputHandler.py
@@ -2,7 +2,6 @@ import bge
 from bge import logic,events
 path=logic.expandPath("//")
 bge.render.showMouse(1)
-#global source,destination,velocity
 def start(cont):
     l_click = cont.sensors['MouseStart']
     m_over = cont.sensors['MouseStart1']
@@ -10,15 +9,6 @@ def start(cont):
     if l_click.positive and m_over.hitObject:
         cont.activate(game_actuator)
         #cont.activate(startGame)
-
-"""def exit(cont):
-    l_click = cont.sensors['MouseExit']
-    m_over = cont.sensors['MouseExit1']
-    game_actuator = cont.actuators['Game']
-    if l_click.positive and m_over.hitObject:
-        cont.activate(game_actuator)
-
-"""
 cont = bge.logic.getCurrentController()
 own = cont.owner
 def main():
@@ -47,18 +37,16 @@ def main():
         input=own["userInput"]
         if len(input)>11:
             return
-            #own["userInput"]+="value excedd"
         if len(input)==3 or len(input)==7:
             own["userInput"]+="\n"
     else:
         return
-    #own.text=own["userInput"]
 def show(cont):
     value=[]
     for sens in cont.sensors:
         if not sens.positive:
             return
-    input=value1    #own["userInput"]
+    input=value1   
     for i in input:
         value.append(i)
     while "\n" in value:
@@ -75,38 +63,3 @@ def show(cont):
 def save():
     file=open(path+"values.txt",'w')
     file.write(source+"\n"+destination+"\n"+velocity)
-"""def back():
-    #sensor=cont.sensors["keyboard"]
-    for key,status in keyEvents.events:
-        if status==bge.logic.KX_INPUT_JUST_ACTIVATED:
-            string=bge.events.EventToString(key)
-            if string=="BACKSPACEKEY":
-                lenString=len(own["userInput"])
-                oldString=own["userInput"]
-                lastChar=oldString[lenString-1]
-                own["userInput"]=oldString.rstrip(lastChar)
-def main():
-    
-    mouseOver1=cont.sensors["mouseOver"]
-    mouseClick=cont.sensors["mouseClick"]
-    startGame=cont.actuators["startGame"]
-    if mouseOver1.positive:
-        own.color=[1,0,0,True]
-        if mouseClick.positive:
-            cont.activate(startGame)
-    else:
-        own.color=[0.8,0.8,0.8,True]
-
-def exit():
-    mouseClick=cont.sensors["mouseClick"]
-    mouseOver=cont.sensors["mouseOver"]
-    gameactu = cont.actuators['Game']
-    if mouseOver.positive:
-        own.color=[1,0,0,True]
-        if mouseClick.positive:
-            #bge.logic.setExitKey(113)
-            cont.activate(gameactu) #
-    else:
-        own.color=[0.8,0.8,0.8,True]
-        """
-
