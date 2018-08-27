573363fab72d94839f1b5e0887462606c3bbc329
amansinghrehal <42092944+amansinghrehal@users.noreply.github.com>
2018-08-27 17:16:27 +0530

    Update pathFinding.py

--- pathFinding.py
+++ pathFinding.py
@@ -6,26 +6,16 @@ def calculate():
     file=open(path+"values.txt",'r')
     info=file.readlines()
     global source,destination,velocity
+    #reading source, destination and velocity
     source=str(info[0])
     destination=str(info[1])
     camera_velocity=str(info[2])
-    print(camera_velocity)
+    #print(camera_velocity)
     cont1 = bge.logic.getCurrentController().actuators
     act=cont1[0]
-    #print(type(destination))
-    #print(source)
-    #print(type("321"))
-    #print(type(destination))
     act.target=destination[0]+destination[1]+destination[2]     #target room
-    #print(act)
-    
-    
     act.velocity=int(camera_velocity)
-    
-    
-    
     scene=bge.logic.getCurrentScene()
-    #print(type(scene.objects['219'])) 
     door=scene.objects[source[0]+source[1]+source[2]]            #starting door
     door_location=door.worldPosition
     #print(door_location)
@@ -42,20 +32,12 @@ def calculate():
     '234': {'235': 1, '233': 1, '236': 2,'223':4,'224':5,'225':6},
     '233': {'234': 1, '235': 2, '236': 3,'223':5,'224':6,'225':7}}
 
-    unvisited = {node: None for node in nodes} #using None as +inf
+    unvisited = {node: None for node in nodes} 
     global visited
     visited = {}
     cont = bge.logic.getCurrentController()
     own = cont.owner
 
-    """sens = cont.sensors['Message']
-    sens1=cont.sensors['Message.001']
-    body = sens.bodies
-    body1=sens1.bodies
-    miles = body[0]
-    miles1=body1[0]"""
-
-    #start=str(miles)
     current = source[0]+source[1]+source[2]
     currentDistance = 0
     unvisited[current] = currentDistance
@@ -77,11 +59,5 @@ def distance():
     cont = bge.logic.getCurrentController()
     own = cont.owner
     new_destination=destination.replace("\n","")
-    #own["userInput"]+=visited[destination[0]+destination[1]+destination[2]]
     own.text=str(visited[new_destination])
-#km = miles * 1.60934
-#own['Text'] = visited[miles1]
-
-
-
     
